f662ce3fcad819f5a20f5424477d7cce2b8d1eae
Marcus Vinicius Carneiro da Silva <marcusvinicius19112004@gmail.com>
2025-07-23 11:36:36 -0300

    "Tarefa 2 concluída"

--- open-music/scripts/script.js
+++ open-music/scripts/script.js
@@ -14,3 +14,27 @@
     toggleBtn.textContent = isDark ? '☀️' : '🌙';
     localStorage.setItem('theme', isDark ? 'dark' : 'light');
   });
+
+const genreButtons = document.querySelectorAll('.genres button');
+
+const albumCards = document.querySelectorAll('.album-card');
+
+genreButtons.forEach(button => {
+  button.addEventListener('click', () => {
+  
+    genreButtons.forEach(btn => btn.classList.remove('active'));
+    
+    button.classList.add('active');
+
+    const selectedGenre = button.textContent.trim();
+
+    albumCards.forEach(card => {
+      const cardGenre = card.querySelector('.genre').textContent.trim();
+      if (selectedGenre === 'Todos' || cardGenre === selectedGenre) {
+        card.style.display = '';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+});
